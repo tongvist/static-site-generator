34212fd0a9ead8c6617b2ff45a3a3666174d57e7
tongvist <67380624+tongvist@users.noreply.github.com>
2024-11-18 20:24:35 +0200

    cleaned up ParentNode.to_html()

--- src/parentnode.py
+++ src/parentnode.py
@@ -2,19 +2,20 @@ from htmlnode import HTMLNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props=None)
+        super().__init__(tag, None, children, props=None)
         self.children = children
 
     def to_html(self):
         if not self.tag:
-            raise ValueError("Missing tag for ParentNode -object")
+            raise ValueError("Invalid HTML: Missing tag for ParentNode -object")
 
         if not self.children:
-            raise ValueError("Missing children for ParentNode -object")
+            raise ValueError("Invalid HTML: Missing children for ParentNode -object")
 
         html_string = f"<{self.tag}>"
+
         for child_node in self.children:
-            html_string += f"{child_node.to_html()}"
+            html_string += child_node.to_html()
 
         return f"{html_string}</{self.tag}>"
 
