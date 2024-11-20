a109db10a5e5992a1982c24ada73d155694b570c
tongvist <67380624+tongvist@users.noreply.github.com>
2024-11-20 19:09:09 +0200

    created split_node_delimiter function and tests

--- src/main.py
+++ src/main.py
@@ -32,5 +32,22 @@ def text_node_to_html_node(text_node):
             raise Exception("Missing url for a image node")
         return LeafNode("img", "", props={"src": text_node.url, "alt":text_node.text})
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    delim_len = len(delimiter)
+
+    for node in old_nodes:
+        sections = node.text.split(delimiter)
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(sections[i], TextType.NORMAL))
+            else:
+                new_nodes.append(TextNode(sections[i], text_type))
+
+    return new_nodes
+
+
 if __name__ == "__main__":
     main()
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 from textnode import TextNode, TextType
 from leafnode import LeafNode
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 import unittest
 
 class TestMain(unittest.TestCase):
@@ -46,6 +46,48 @@ class TestMain(unittest.TestCase):
         with self.assertRaises(AttributeError) as e:
             text_node_to_html_node(node)
         self.assertTrue("invalid text type" in str(e.exception).lower())
+
+    def test_split_nodes_delimiter_code_in_middle(self):
+        code_node = TextNode("This has a `code block` in the middle", TextType.NORMAL)
+        expected = [
+                        TextNode("This has a ", TextType.NORMAL), 
+                        TextNode("code block", TextType.CODE), 
+                        TextNode(" in the middle", TextType.NORMAL)
+                    ]
+        self.assertEqual(split_nodes_delimiter([code_node], "`", TextType.CODE), expected)
+
+    def test_split_nodes_delimiter_bold_in_middle(self):
+        bold_node = TextNode("This has a **bold word** in the middle", TextType.BOLD)
+        expected_bold = [
+                        TextNode("This has a ", TextType.NORMAL), 
+                        TextNode("bold word", TextType.BOLD), 
+                        TextNode(" in the middle", TextType.NORMAL)
+                    ]
+        self.assertEqual(split_nodes_delimiter([bold_node], "**", TextType.BOLD), expected_bold)
+
+    def test_split_nodes_delimiter_starts_with_tag(self):
+        starts_with_tag = TextNode("**This** starts with a bold word", TextType.BOLD)
+        expected_bold_2 = [
+                            TextNode("This", TextType.BOLD), 
+                            TextNode(" starts with a bold word", TextType.NORMAL), 
+                    ]
+        self.assertEqual(split_nodes_delimiter([starts_with_tag], "**", TextType.BOLD), expected_bold_2)
+
+    def test_split_nodes_delimiter_starts_with_short_tag(self):
+        starts_with_tag = TextNode("`This starts` with a code block", TextType.CODE)
+        expected = [
+                    TextNode("This starts", TextType.CODE), 
+                    TextNode(" with a code block", TextType.NORMAL), 
+                    ]
+        self.assertEqual(split_nodes_delimiter([starts_with_tag], "`", TextType.CODE), expected)
+
+    def test_split_nodes_delimiter_ends_with_tag(self):
+        ends_with_tag = TextNode("This ends with a *italic block*", TextType.ITALIC)
+        expected = [
+                    TextNode("This ends with a ", TextType.NORMAL), 
+                    TextNode("italic block", TextType.ITALIC), 
+                    ]
+        self.assertEqual(split_nodes_delimiter([ends_with_tag], "*", TextType.ITALIC), expected)
 
 if __name__ == "__main__":
     unittest.main()
